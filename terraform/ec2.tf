57eff8152d3694a388a9c3c515cd70fa94b21c03
Gordon Adams <Gordon@Michelles-MacBook-Air.local>
2026-06-21 15:04:17 +1200

    destroyed ec2 instance

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "ubuntu" {
-  ami           = "ami-06259b63260eddc13" # ubuntu 26.04 lts
-  instance_type = "t3.micro"
+# resource "aws_instance" "ubuntu" {
+#   ami           = "ami-06259b63260eddc13" # ubuntu 26.04 lts
+#   instance_type = "t3.micro"
 
-  tags = {
-    Name = "ubuntu"
-  }
-}
+#   tags = {
+#     Name = "ubuntu"
+#   }
+# }
